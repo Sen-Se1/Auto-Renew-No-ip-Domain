77d09a49302be9338469e355b485139ab0815dd1
Sen-Se1 <mbarkihoussem99@gmail.com>
2026-09-15 22:55:48 +0100

    fix: update v0.1.0

--- scripts/start-browser.sh
+++ scripts/start-browser.sh
@@ -23,7 +23,6 @@ docker run -d \
     --name "$CHROMIUM_NAME" \
     --hostname "$CHROMIUM_NAME" \
     --restart unless-stopped \
-    -p 3001:3001 \
     -p 9222:9222 \
     -p 9223:9223 \
     --shm-size 1gb \
@@ -31,7 +30,6 @@ docker run -d \
     -e "PGID=1000" \
     -e "PUID=1000" \
     -e "TZ=Etc/UTC" \
-    --memory 7794m \
     "$CHROMIUM_IMAGE"
 
 if [ $? -ne 0 ]; then
